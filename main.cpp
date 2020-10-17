5e7c9430ce20c2c53d7898641f76dfa6f4fd770b
pro-30 <37265520+pro-30@users.noreply.github.com>
2020-10-17 10:03:52 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,61 @@
+
+
+#include <algorithm>
+#include <iterator>
 #include <iostream>
+#include <sstream>
+#include <bitset>
+#include <string>
 #include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <map>
+#include <set>
+#include <time.h>
+#include <limits>
+#include <math.h>
+#include <climits>
+#include <numeric>
+#include <utility>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <functional>
+#include<unordered_map>
+ 
 
 using namespace std;
+//debugger start
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+//debugger end
 
 class BigInteger
 {
